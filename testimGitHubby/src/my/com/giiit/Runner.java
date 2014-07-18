f01d4216715d14c63a7d38ebfa17aea18e6c277c
Администратор <@home-c232954fd0>
2014-07-18 12:10:50 +0300

    3 testing commit

---
+++ testimGitHubby/src/my/com/giiit/Runner.java
@@ -0,0 +1,15 @@
+package my.com.giiit;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		System.out.println("--------START-------");
+		for (int i=0; i<10; i++) {
+			System.out.print("Number >>" + i + "<<" + "---DDDDDD---");
+			System.out.println("eeeeeeeeeeeeee");
+		}
+		System.out.println("--------FINISH-------");
+		
+	}
+
+}
