c4f5db8aa6b0e0f98ddf8daaff744f8b66f08668
Администратор <@home-c232954fd0>
2014-07-18 11:52:28 +0300

    First test

---
+++ testimGitHubby/src/testimGitHubby/Runner.java
@@ -0,0 +1,11 @@
+package testimGitHubby;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		for (int i=0; i<15; i++) {
+			System.out.println("Number >>" + i + "<<");
+		}
+	}
+
+}
